88ff0cd8cc789b4f3dbb7d6ba2409239959b34ac
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-31 15:33:33 -0700

    content added

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,13 +4,21 @@ import Link from "next/link";
 function NavBar() {
   return (
     <div>
-      <nav>
-        <h1>Raine Gauthier Photography</h1>
-        <ul>
+      <nav className="uppercase flex flex-col justify-center items-center border-b-2 pb-2">
+        <h1 className="text-center my-[5rem] text-5xl">
+          Raine Gauthier <br /> Photography
+        </h1>
+        <ul className="flex justify-evenly w-[60%]">
           <li>
             <Link href="/">overview</Link>
+          </li>
+          <li>
             <Link href="/">about</Link>
+          </li>
+          <li>
             <Link href="/">contact</Link>
+          </li>
+          <li>
             <Link href="/">galleries</Link>
           </li>
         </ul>
