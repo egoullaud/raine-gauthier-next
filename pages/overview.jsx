49cfb505e049c7d13fe262b90128aefcb85ad1b6
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-31 16:31:53 -0700

    added overview page

---
+++ pages/overview.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Hero from "@/components/overview/Hero";
+import Services from "@/components/overview/Services";
+
+export default function Overview() {
+  return (
+    <div>
+      <Hero />
+      <Services />
+    </div>
+  );
+}
