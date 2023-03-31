bc0217d8a793d2239ace77cbe20b448db42987ae
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-31 16:43:01 -0700

    font color added

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,11 +3,17 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <div className="mx-2">
+    <div className="mx-2 text-[#363636]">
       <nav className="uppercase flex flex-col justify-center items-center border-b-2 border-[#1f1f1f] pb-2">
-        <h1 className="text-center my-[5rem] text-5xl">
-          Raine Gauthier <br /> Photography
-        </h1>
+        <div className="text-center my-[6rem]">
+          <Link href="/">
+            <h1 className="text-center  text-7xl leading-none">
+              Raine Gauthier
+            </h1>
+            <h2 className="text-4xl">Photography</h2>
+          </Link>
+        </div>
+
         <ul className="flex justify-evenly w-[60%] lg:text-xl">
           <li>
             <Link href="/">overview</Link>
