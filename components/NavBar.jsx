67f6ea8a954c079815d98e2de0463997f9d6a8da
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-31 15:19:33 -0700

    added links

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
 function NavBar() {
-  return <div>Raine Gauthier Photography</div>;
+  return (
+    <div>
+      <nav>
+        <h1>Raine Gauthier Photography</h1>
+        <ul>
+          <li>
+            <Link href="/">overview</Link>
+            <Link href="/">about</Link>
+            <Link href="/">contact</Link>
+            <Link href="/">galleries</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
