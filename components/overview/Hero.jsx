6a23a4c33561f048d731386767dda89a90a9943c
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-31 16:43:07 -0700

    quote formatted

--- components/overview/Hero.jsx
+++ components/overview/Hero.jsx
@@ -4,14 +4,14 @@ import heroImg from "../../public/hero/hero-img.jpg";
 
 function Hero() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center">
       {/* Hero area */}
-      <div className="text-xl my-[3rem] flex justify-center items-center text-center">
-        <p>
+      <div className="text-3xl my-[5rem] flex justify-center items-center text-center w-[50%]">
+        <p className="italic ">
           “When words become unclear, I shall focus with photographs. When
           images become inadequate, I shall be content with silence.”
           <br />
-          <span>— Ansel Adams</span>
+          <span className="not-italic">— Ansel Adams</span>
         </p>
       </div>
       <Image src={heroImg} />
