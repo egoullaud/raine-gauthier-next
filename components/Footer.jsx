e0400ebf8eb77dca0ad3804df42ba70c0be79c13
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-31 16:47:19 -0700

    link added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,15 +6,17 @@ function Footer() {
     <div className="flex flex-col lg:flex-row justify-around border-t-2 border-[#1f1f1f] uppercase">
       <div className="lg:w-[33%] lg:py-[3rem] flex-col flex justify-center items-center">
         <h4 className="text-center py-5 underline text-xl">Follow Me</h4>
-        <ul className="flex justify-around w-[100%]">
+        <ul className="flex justify-around w-[60%]">
           <li>facebook</li>
           <li>instagram</li>
         </ul>
       </div>
       <div className="lg:w-[33%] lg:py-[3rem] lg:border-x-2 border-[#1f1f1f] my-2 flex-col flex justify-center items-center">
-        <h4 className="text-center py-5 underline text-xl">
-          Raine Gauthier Photography
-        </h4>
+        <Link href="/">
+          <h4 className="text-center py-5 underline text-xl">
+            Raine Gauthier Photography
+          </h4>
+        </Link>
         <p>back to top</p>
       </div>
       <div className="lg:w-[33%] lg:py-[3rem] flex-col flex justify-center items-center">
