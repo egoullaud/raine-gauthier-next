681ecd69270a444752b0c8eb781844973c89a4c2
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-31 18:15:44 -0700

    styling added

---
+++ components/overview/Galleries.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Galleries() {
+  return (
+    <div>
+      <h3 className="uppercase text-3xl text-center my-[3rem]">
+        Check out my work
+      </h3>
+      <div></div>
+    </div>
+  );
+}
+
+export default Galleries;
