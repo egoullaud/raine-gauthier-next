ab2082b9ef04f2d4bd70f0636820a215fdfc8588
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-31 18:16:08 -0700

    galleries added

--- pages/overview.jsx
+++ pages/overview.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Hero from "@/components/overview/Hero";
 import Services from "@/components/overview/Services";
+import Galleries from "@/components/overview/Galleries";
 
 export default function Overview() {
   return (
     <div>
       <Hero />
       <Services />
+      <Galleries />
     </div>
   );
 }
