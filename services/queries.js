aee8dab629142645d1f6bf4b293700428ef4b60d
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 16:48:52 -0700

    added new queries

--- services/queries.js
+++ services/queries.js
@@ -1,1 +1,39 @@
+export const GET_ALL_QUERY = gql`
+  query Categories {
+    categories {
+      title
+      id
+      slug
+      images {
+        title
+        id
+        slug
+        image {
+          url
+          id
+        }
+      }
+    }
+  }
+`;
 
+export const GET_ALL_CATEGORIES = gql`
+  query Categories {
+    categories {
+      title
+      id
+      slug
+    }
+  }
+`;
+
+export const CATEGORY_BY_SLUG = gql`
+  query categoryBySlug($slug: String!) {
+    category(where: { slug: $slug }) {
+      title
+      images {
+        title
+      }
+    }
+  }
+`;
