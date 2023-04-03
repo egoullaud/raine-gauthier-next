483846499ec21a01d3b88f830e874953a87f91d3
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 15:46:29 -0700

    formatted to screens

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,18 +3,22 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <div className="mx-2 text-[#363636]">
+    <div className="mx-2 text-[#363636] mb-[1rem]">
       <nav className="uppercase flex flex-col justify-center items-center border-b-2 border-[#1f1f1f] pb-2">
-        <div className="text-center my-[6rem]">
+        <div
+          className="text-center my-[2rem]
+                      md:my-[3rem]
+                        lg:my-[4rem]"
+        >
           <Link href="/">
-            <h1 className="text-center  text-7xl leading-none">
+            <h1 className="text-center text-3xl md:text-5xl lg:text-7xl leading-none">
               Raine Gauthier
             </h1>
-            <h2 className="text-4xl">Photography</h2>
+            <h2 className=" text-xl md:text-3xl lg:text-4xl">Photography</h2>
           </Link>
         </div>
 
-        <ul className="flex justify-evenly w-[60%] lg:text-xl">
+        <ul className=" hidden md:flex justify-evenly w-[60%] lg:text-xl">
           <li>
             <Link href="/">overview</Link>
           </li>
