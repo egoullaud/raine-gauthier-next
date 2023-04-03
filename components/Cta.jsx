c8165857706604d3250c774cbae20b39ce5cddbd
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 15:39:23 -0700

    formatted

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 function Cta() {
   return (
-    <div className="flex flex-col md:flex-row justify-around items-center mt-[4rem] mb-[2rem] text-xl lg:text-2xl w-[50%]">
+    <div className="flex flex-row justify-around items-center mt-[1rem] md:mt-[4rem] md:mb-[2rem] text-xl lg:text-2xl md:w-[50%]">
       <Link href="/about">
-        <button className="mx-5 uppercase hover:transition-all hover:underline hover:duration-1000">
+        <button className="mx-5 my-1 uppercase hover:transition-all hover:underline hover:duration-1000">
           Meet Raine
         </button>
       </Link>
       <Link href="/contact">
-        <button className="mx-5 uppercase hover:transition-all hover:underline hover:duration-1000">
+        <button className="mx-5 my-1 uppercase hover:transition-all hover:underline hover:duration-1000">
           Get in Touch
         </button>
       </Link>
