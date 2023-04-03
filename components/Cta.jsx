dfefb8f3ae005184a329ada918d11212139b9268
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 15:54:27 -0700

    removed my

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 function Cta() {
   return (
-    <div className="flex flex-row justify-around items-center mt-[1rem] md:mt-[4rem] md:mb-[2rem] text-xl lg:text-2xl md:w-[50%]">
+    <div className="flex flex-row justify-around items-center text-xl lg:text-2xl md:w-[50%]">
       <Link href="/about">
-        <button className="mx-5 my-1 uppercase hover:transition-all hover:underline hover:duration-1000">
+        <button className="mx-5 my-1 uppercase hover:transition-all hover:underline hover:duration-1000 tracking-wide">
           Meet Raine
         </button>
       </Link>
       <Link href="/contact">
-        <button className="mx-5 my-1 uppercase hover:transition-all hover:underline hover:duration-1000">
+        <button className="mx-5 my-1 uppercase hover:transition-all hover:underline hover:duration-1000 tracking-wide">
           Get in Touch
         </button>
       </Link>
