55b4d4fda7e8ed3a54f13fe8b9fff6fe6536c883
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 18:44:31 -0700

    tracking-wider

--- components/overview/Galleries.jsx
+++ components/overview/Galleries.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Galleries() {
   return (
     <div className="mx-2">
-      <h3 className="uppercase text-3xl text-center my-[2rem]  border-b-2 border-[#363636]">
+      <h3 className="uppercase text-3xl text-center my-[2rem] tracking-wider border-b-2 border-[#363636]">
         Galleries
       </h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
