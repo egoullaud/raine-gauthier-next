72591c88ee308a9fe230fa4b9f12352577698dae
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 17:52:35 -0700

    formatted

--- components/overview/Services.jsx
+++ components/overview/Services.jsx
@@ -6,30 +6,34 @@ import Cta from "../Cta";
 
 function Services() {
   return (
-    <div className="bg-[#363636] text-white py-[2rem] flex flex-col justify-center items-center">
-      <div className="flex flex-col md:flex-row justify-around items-center xl:justify-center">
+    <div className="flex flex-col mx-2">
+      <h1 className="uppercase text-3xl text-center mt-[2rem] border-b-2 border-[#363636]">
+        services
+      </h1>
+
+      <div className="flex flex-col md:flex-row justify-around items-start xl:justify-center mt-[2rem]">
         <div
           className="flex flex-col justify-center items-center ml-4
           md:w-[45%] 
           lg:w-[50%]
           xl:w-[35%] "
         >
-          <Image src={photography} className=" w-[100%] object-cover" />
+          <h3 className=" text-2xl font-bold pb-2  uppercase tracking-widest">
+            Photography
+          </h3>
+          <p className="w-[90%] text-center tracking-widest mt-4 mb-4">
+            I offer a wide range of services, including events, shows,
+            festivals, headshots, couple and family portraits.
+          </p>
+          {/* <Image src={photography} className=" w-[100%] object-cover" /> */}
         </div>
 
         <div
-          className="flex flex-col justify-center items-center py-2
+          className="flex flex-col justify-center items-center pb-2
           md:w-[60%]
           lg:w-[50%]
           xl:w-[40%]"
         >
-          <h3 className=" text-2xl font-bold pb-2  uppercase tracking-widest">
-            Photography
-          </h3>
-          <p className="w-[90%] text-center tracking-widest mb-4">
-            I offer a wide range of services, including events, shows,
-            festivals, headshots, couple and family portraits.
-          </p>
           <h3 className=" text-2xl font-bold pb-2  uppercase tracking-widest">
             Videography
           </h3>
@@ -40,7 +44,7 @@ function Services() {
           </p>
         </div>
       </div>
-      <Cta />
+      {/* <Cta /> */}
     </div>
   );
 }
