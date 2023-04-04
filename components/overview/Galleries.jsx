1c8192921d05afcdfdd1542f9ca53f4259ccd1e5
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 17:52:30 -0700

    formatted header

--- components/overview/Galleries.jsx
+++ components/overview/Galleries.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function Galleries() {
   return (
-    <div>
-      <h3 className="uppercase text-3xl text-center my-[3rem]">Galleries</h3>
+    <div className="mx-2">
+      <h3 className="uppercase text-3xl text-center my-[2rem]  border-b-2 border-[#363636]">
+        Galleries
+      </h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         <div></div>
       </div>
