c11530379f2f753c8545e5a43311088ef4f06844
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-04 18:05:03 -0700

    removed div

--- components/overview/Galleries.jsx
+++ components/overview/Galleries.jsx
@@ -6,9 +6,7 @@ function Galleries() {
       <h3 className="uppercase text-3xl text-center my-[2rem] tracking-wider border-b-2 border-[#363636]">
         Galleries
       </h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        <div></div>
-      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4"></div>
     </div>
   );
 }
