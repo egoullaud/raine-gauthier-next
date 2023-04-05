85f2dec9edbf03d26c71e087b9653083b1a08cf9
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-04 18:05:30 -0700

    added graphql-request

--- pages/overview.jsx
+++ pages/overview.jsx
@@ -1,10 +1,36 @@
 import React from "react";
-
+import Galleries from "@/components/overview/Galleries";
 import Hero from "@/components/overview/Hero";
 import Services from "@/components/overview/Services";
-import Galleries from "@/components/overview/Galleries";
+import { GraphQLClient, gql } from "graphql-request";
 
-export default function Overview() {
+// Create a GraphQL client with the endpoint
+const hygraph = new GraphQLClient(
+  "https://api-ca-central-1.hygraph.com/v2/clg1dpl1r89y501ue0ava00ft/master"
+);
+
+const GET_ALL_CATEGORIES = gql`
+  {
+    categories {
+      title
+      slug
+      id
+    }
+  }
+`;
+
+export async function getServerSideProps() {
+  const { categories } = await hygraph.request(GET_ALL_CATEGORIES);
+
+  return {
+    props: {
+      categories,
+    },
+  };
+}
+
+export default function Overview({ categories }) {
+  console.log(categories);
   return (
     <div>
       <Hero />
